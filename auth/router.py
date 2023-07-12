e90b6fa1fe70c81c1dac1807b251c9cc7eb56ae7
SyedAli1412 <syed.ali@ocloudsolutions.net>
2023-07-12 18:12:07 +0500

    AuthContext and Exceptions and access token gen

--- auth/router.py
+++ auth/router.py
@@ -5,6 +5,11 @@ from sqlalchemy.orm import Session
 from users.models import Users
 from common.responses.responses import success_response
 from .context.bcrypt import get_password_hash
+from fastapi.security import OAuth2PasswordRequestForm
+from .context.authenticate import authenticate_user
+from datetime import timedelta
+from .exceptions.token_exception import token_exception
+from .context.token_generate import create_access_token
 
 
 router = APIRouter(
@@ -22,3 +27,17 @@ async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)
     db.add(create_user_model)
     db.commit()
     return success_response(201)
+
+
+@router.post("/login")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
+                                 db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise token_exception()
+    token_expires = timedelta(minutes=20)
+    token = create_access_token(
+        user.username, user.id, expires_delta=token_expires)
+    return {
+        "token": token
+    }
